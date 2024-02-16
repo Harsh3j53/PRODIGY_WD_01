b2276880c9bcaa27e1760c9df9dafaee6f44d263
Harsh3j53 <harsh3j53@gmail.com>
2024-02-16 23:44:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function scrollToHome() {
+    const footer = document.getElementById('hero');
+    footer.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToService() {
+    const footer = document.getElementById('Service');
+    footer.scrollIntoView({ behavior: 'smooth' });
+}
+function scrollToFooter() {
+    const footer = document.getElementById('Footer');
+    footer.scrollIntoView({ behavior: 'smooth' });
+}
